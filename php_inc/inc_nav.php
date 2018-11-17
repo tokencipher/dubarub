e7ee9d4bed8441b4efbbaba2f964f0ed0f701068
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-16 20:42:35 -0500

    Remove 'add post' link

---
+++ php_inc/inc_nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div id="header" class="w3-container w3-white" style="opacity:0.6;">
+      <img src="img/dubarub.jpg" alt="dubarub" id="dub" height="40" width="47"  class="w3-circle"/>
+      <span id="logoTitle"> alpha v2.0</span>
+      <div class="dropdown">
+        <button class="btn  btn-lg  dropdown-toggle" id="dropdownMenu1" type="button"  aria-label="Dropdown Menu">
+        <span class="glyphicon glyphicon-menu-hamburger"></span>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+          <!--<li><a onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="index.php">Home</a></li>-->
+          <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./video_preview.php">Video</a></li>-->
+          <li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" id="music-link" href="#music-dialog">Music</a></li>
+          <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./gallery.php">Gallery</a></li>-->
+          <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="../working.php">Tags</a></li>-->
+          <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./working.php">About</a></li>-->
+          <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="../working.php">Contact</a></li>-->
+        </ul>
+      </div>
+    </div>
+</nav>
+
+
+
