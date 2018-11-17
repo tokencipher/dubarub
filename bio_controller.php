8e92191aa0557440b5d1cdbf74f1bb3b9eed2045
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-17 17:05:09 -0500

    Update user_id value

--- bio_controller.php
+++ bio_controller.php
@@ -9,8 +9,7 @@ include('php_inc/inc_db_qp4.php');
 if (isset($_POST['update_bio'])) {
   if (!empty($_POST['update_bio'])) {
     $bio = $_POST['update_bio'];
-    $user_id = $GET['id'];
-    //$bio = addslashes($bio);
+    $user_id = $_POST['u_id'];
     if ($conn !== FALSE) {
       $table = "user";
       $sql = "UPDATE $table SET bio = :bio WHERE u_id = :user_id";
