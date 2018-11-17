b8033f5183620db6867537c94fb44dbfc9df9fc0
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-17 18:50:54 -0500

    Modify sql statement

--- get_index_posts.php
+++ get_index_posts.php
@@ -6,7 +6,7 @@ include ("php_inc/inc_db_qp4.php");
    if ($conn !== FALSE) {
      $user_id = $_GET['id'];
      $table = "post";
-     $sql = "SELECT u_id, p_id, avatar, thumbnail, title, photo_url, video_url, video_mp4, image, video, external, file_size, likes, comments, photo_cred, entry, created_at, user_name, display, mime_type FROM $table WHERE u_id=$user_id";
+     $sql = "SELECT u_id, p_id, avatar, thumbnail, title, photo_url, video_url, video_mp4, image, video, external, file_size, likes, comments, photo_cred, entry, created_at, user_name, display, mime_type FROM $table WHERE display = 'true' && u_id = $user_id";
      $object = array();
      $x = 0;
      foreach ($conn->query($sql) as $row) {
