f03e1abee59ac72830d63000c4ad56fc87c390f4
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-17 18:03:01 -0500

    Close db connection

--- bio_controller.php
+++ bio_controller.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * Start the session.
- */
-//session_start();
+
 header('Content-Type: application/json;charset=utf-8');
 include('php_inc/inc_db_qp4.php');
 
@@ -33,7 +30,7 @@ foreach ($conn->query($sql) as $row) {
 
 // Send the data back to the caller
 $myObj = json_encode($object);
-echo  $myObj
-
+echo $myObj;
+$conn = null;
 
 ?>
