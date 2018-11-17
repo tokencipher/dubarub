1941b13ea3b9ad2ed3d7d4ba6cc0c58e92bcfbfe
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-17 15:39:08 -0500

    Update header reference

--- verify_login.php
+++ verify_login.php
@@ -136,18 +136,8 @@ echo "<p>$password</p>";
   
 ?>
 <!-- Author: Bryan Thomas -->
-<!-- Last modified: 01/28/2018 -->
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/index.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<title>Login</title>
+<!-- Last modified: 11/17/2018 -->
+<?php require_once('php_inc/inc_header.php'); ?>
 <style>
   .heading {
     position:relative;
@@ -165,7 +155,7 @@ echo "<p>$password</p>";
   <?php echo $result; ?>
 </div>
 </body>
-<?php include("php_inc/inc_footer.php"); ?>
+<?php include_once('php_inc/inc_footer.php'); ?>
 
 
 
