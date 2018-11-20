a9ce6d818387834e5500710752258487380595e3
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-11-19 21:19:45 -0500

    Update reference to header

--- music_form.php
+++ music_form.php
@@ -365,19 +365,8 @@ if ($error_count == 0) {
 ?>
 
 <!-- Author: Bryan Thomas -->
-<!-- Last modified: 10/11/2018 -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/index.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Last modified: 11/18/2018 -->
+<?php require_once('php_inc/inc_header.php'); ?>
 <title>Upload track(s)</title>
 <?php include_once('php_inc/inc_user_nav.php'); ?>
 </head>
