74acc8f73ae5e520786efb42ea27f79f47bae55b
Caesar Marcion <ximarcion@00001101.local>
2018-12-11 14:14:05 -0500

    Modify if statement in loadComments function to compare value

--- user.php
+++ user.php
@@ -516,14 +516,14 @@
         if (len > 0) {
           console.log("comment user id: " + comments[0].u_id + " and current user: " + user_id);
           for ( var i = 0; i < len; i++) {
-            if ( comments[i].u_id === user_id ) {
+            if ( comments[i].u_id == user_id ) {
               $('#post' + comments[i].p_id).find('.post_comments').append('<div id="comment' + comments[i].c_id +  '" class="media">' +
   			  '<div class="media-left">' + 
   			  '<a href="#"><img height="64" width="64" class="media-object" src="' + comments[i].avatar + '" alt="user avatar"></a>' +
  			  '</div><div style="position:relative;top:-5px;text-align:left;" class="media-body"><div id="commenter" style="font-size:14px;" class="media-heading"><b><a id="comment_owner_link" href="user.php?name=' + comments[i].user_name + '">' + comments[i].user_name + '</a></b> says:</div>' + 
  			  '<div id="comment_body" style="margin-bottom:2px;font-size:12px">' + comments[i].comment + '</div>' + 
  			  '<div id="remove_comment" style="position:relative;bottom:22px;margin-right:10px;float:right"><a id="deleteComment" href="user.php?action=Delete%20Comment&commID="' + comments[i].c_id + '">' + 
- 			  '<i class="fa fa-times" style="color:red"  aria-hidden="true"></i></a></div>' + 
+ 			  '<i class="fa fa-times" style="color:red" aria-hidden="true"></i></a></div>' + 
  			  '<div id="comment_options" style="clear:both;font-size:12px" class="flex-container">' + 
  			  '<div id="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
  			  '</div></div>');
