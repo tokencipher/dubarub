3b8f5391eebcac6f7be088cfd33860df095a7d9e
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-12-12 00:08:56 -0500

    Create page for user action routes

---
+++ user_action.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Start the session
+ */
+session_start();
+
+header('Content-Type: application/json;charset=utf-8');
+
+  if (isset($_POST['user_action'])) {
+     
+    switch ( $_POST['action'] ) {
+     
+      case 'Upvote':
+        if (isset($_SESSION['user_id'])) {
+          $u_id = $_SESSION['user_id'];
+          $c_id = $_POST['comment_id'];
+          $commentObj = new PostComment();
+          $upvote_flag = $commentObj->getUpvoteFlag($u_id, $c_id);
+        
+          if ($upvote_flag == "true") {
+            return;
+          }
+          
+          $commentObj->setUpvoteFlag($u_id, $c_id, "true");
+          
+          $myObj = array();
+          $trophy_count = $commentObj->getUpvote($c_id);
+          $myObj['trophy_count'] = $trophy_count;
+        }
+        
+        break;
+        
+      case 'Delete Comment':
+        $c_id = $_GET['commid'];
+        $commentObj = new PostComment();
+        $commentObj->deleteComment($c_id);
+        break;
+    }
+  }   
+  
+$myObj = json_encode($myObj);
+echo $myObj;
+
+?>
