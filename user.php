0963ef997beb38bde3021dafaf2b80a7736d35dd
Caesar Marcion <ximarcion@00001101.local>
2018-12-12 12:42:04 -0500

    Define flagComment function

--- user.php
+++ user.php
@@ -692,7 +692,42 @@
 	  }
     }     
   }
-    
+  
+  function flagComment(element) {
+    console.log("flag clicked");
+    var flag_comment = "<?php (isset($_SESSION['user_id']) ? true : false); ?>";
+    	
+    if (flag_comment == "true") {
+    	
+      console.log("Should we flag comment?... " + flag_comment);
+      var flag = $( element );
+	  var commID = flag.data("commid");
+	  var uid = flag.data("uid");
+		
+	  var action = "Flag Comment";
+		
+	  $.ajax({
+        async: true,
+      	cache: false,
+        url: 'user_action.php',  
+        type: 'POST',
+        data: { user_action: action, comment_id: commID }  
+      }).done(function ( msg ) {
+        console.log('Flag comment action taken...');
+        console.log(msg);
+      }).fail(function ( xhr, textStatus) {
+        console.log(xhr.statusText);
+      });
+        
+    } else {
+      if (confirm("You must be logged in to report this comment. Sign up/Login?")) {
+  	    window.location.assign("https://dubarub.com");
+	  } else {
+  		return;
+	  }
+    }     
+  }
+  
   $(document).ready(function() { 
   
     sequenceAsync();
