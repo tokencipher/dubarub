13ad1e5a32e506890fd51400b4e02d1a8cd61d14
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-12-12 00:07:11 -0500

    Log json received from ajax request in trophy click handler

--- user.php
+++ user.php
@@ -544,8 +544,7 @@
  			  '<div id="comment_options" style="clear:both;font-size:12px" class="flex-container">' + 
  			  '<div id="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
  			  '<div id="upvote">' + comments[i].upvote + " " + trophyAmount + '</div>' +
- 			  '<div id="trophy"><a href="#">' + 
- 			  '<i class="fa fa-trophy" style="color:#b36b00" aria-hidden="true"></i></a></div>' +
+ 			  '<div class="trophy" data-commid="' + comments[i].c_id + '" data-uid="' + comments[i].u_id + '"><i class="fa fa-trophy" style="color:#b36b00" aria-hidden="true"></i></a></div>' +
  			  '</div></div>');
  			}
           }
@@ -734,6 +733,43 @@
         // Connection was closed.
       }
     }, false);
+    
+    // Add trophy click event handler
+    $(".trophy").on("click", function() {
+    	var handTrophy = "<?php (isset($_SESSION['user_id']) ? true : false); ?>";
+    	
+    	if (handTrophy == "true") {
+    	
+    	  console.log("Should we hand trophy?... " + handTrophy);
+          var elem = $( this );
+		  var commID = elem.data("commid");
+		  var uid = elem.data("uid");
+		
+		  var action = "Upvote";
+		
+		  $.ajax({
+            async: true,
+      	    cache: false,
+            url: 'user_action.php',  
+            type: 'POST',
+            data: { user_action: action, comment_id: commID }  
+          }).done(function ( msg ) {
+            console.log('Upvote action taken...');
+            console.log(msg);
+          }).fail(function ( xhr, textStatus) {
+            console.log(xhr.statusText);
+          });
+        
+        } else {
+          if (confirm("You must be logged in to give a trophy. Sign up/Login?")) {
+  		    window.location.assign("https://dubarub.com");
+		  } else {
+  			return;
+		  }
+        }
+        
+    });
+		
     
   
     $('#flashback').click(function() {
