ed5a1ffac8705b89fd0e495b335cd6398ac14c3d
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-12-12 23:42:19 -0500

    Retrieve user id of comment made

--- get_user_comments.php
+++ get_user_comments.php
@@ -11,13 +11,13 @@ require_once('php_inc/inc_db_qp4.php');
     $table = "post_comment";
     $user_id = $_SESSION['user_id'];
     // Retrieve the data
-    $sql = "SELECT p_id, c_id, post_owner, user_name, avatar, timestamp, report, upvote, comment FROM $table WHERE display = 'true' && post_owner = $user_id";
+    $sql = "SELECT u_id, p_id, c_id, user_name, avatar, timestamp, report, upvote, comment FROM $table WHERE display = 'true' && post_owner = $user_id";
     $object = array();
     $x = 0;
     foreach ($conn->query($sql) as $row) {
+      $object[$x]['u_id'] = "{$row['u_id']}";
       $object[$x]['p_id'] = "{$row['p_id']}";
       $object[$x]['c_id'] = "{$row['c_id']}";
-      $object[$x]['post_owner'] = "{$row['post_owner']}";
       $object[$x]['user_name'] = "{$row['user_name']}";
       $object[$x]['avatar'] = "{$row['avatar']}";
       $object[$x]['timestamp'] = "{$row['timestamp']}";
