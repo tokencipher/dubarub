adcda1c50c06a118ad33d388f7b3af361cc562d4
Caesar Marcion <ximarcion@00001101.fios-router.home>
2018-12-13 00:52:27 -0500

    Modify handCommentTrophy function

--- user.php
+++ user.php
@@ -695,11 +695,10 @@
   
   function handCommentTrophy(element) {
     console.log("comment trophy clicked");
-    var hand_trophy = "<?php (isset($_SESSION['user_id']) ? true : false); ?>";
+    var hand_trophy = Boolean("<?php echo (isset($_SESSION['user_id']) ? true : false); ?>");
     	
-    if (hand_trophy == "true") {
-    	
-      console.log("Should we hand trophy?... " + hand_trophy);
+    if (hand_trophy === true) {
+
       var trophy = $( element );
 	  var commID = trophy.data("commid");
 		
