d66c73feacf1cd31949dddaf03e379b4ec4d398e
Caesar Marcion <ximarcion@00001101.local>
2018-12-14 13:18:55 -0500

    Remove setOnlineFlag function call on object

--- logout.php
+++ logout.php
@@ -3,13 +3,8 @@
 require_once("php_class/class_Session.php");
 
 session_start();
-$online_flag = new Session();
-$online_flag->setUserID($_SESSION['user_id']);
-
 $_SESSION = array();
 session_destroy();
-
-$online_flag->setOnlineFlag("false");
 
 header('Location: index.php');
 
