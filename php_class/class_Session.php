7690ca90da92bf773af85dd2825795579a554620
Caesar Marcion <ximarcion@00001101.local>
2018-12-14 10:58:22 -0500

    Implement Session class

---
+++ php_class/class_Session.php
@@ -0,0 +1,48 @@
+<?php
+  
+class Session {
+  private $db;
+  private $u_id;
+  private $user_name;
+  private $logged_in;
+  
+  public function __construct() {
+    include ("php_inc/inc_db_qp4.php");
+    if ($conn !== FALSE) {
+      $this->db = $conn;
+    }
+  }
+  
+  public function __destruct() {
+    $this->db = null;
+  }
+  
+  // specify which data members of the class to serialize
+  public function __sleep() {}
+  
+  // initialize any data members that were not saved with the serialization process
+  public function __wakeup() {}
+  
+  public function dbClose() {
+    $this->db = null;
+  }  
+  
+  public function logUser($u_id, $user_name) {
+    $table = "sessions";
+    
+    $sql = "INSERT INTO $table(u_id, user_name) VALUES(:user_id, :user_name)";
+    $stmt = $this->db->prepare($sql);
+    
+    $stmt->bindParam(':user_id', $u_id);
+    $stmt->bindParam(':user_name', $user_name);
+    $stmt->execute(); 
+  }
+  
+  
+}
+
+
+
+
+
+?>
