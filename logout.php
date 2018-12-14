c04077f221abfa9704bf9f92942e8945c197d153
Caesar Marcion <ximarcion@00001101.local>
2018-12-14 11:31:48 -0500

    Set online flag to "false" upon user logout

--- logout.php
+++ logout.php
@@ -1,8 +1,16 @@
 <?php
 
+require_once("php_class/class_Session.php");
+
 session_start();
+$online_flag = new Session();
+$online_flag->setUserID($_SESSION['user_id']);
+
 $_SESSION = array();
 session_destroy();
+
+$online_flag->setOnlineFlag("false");
+
 header('Location: index.php');
 
 ?>
