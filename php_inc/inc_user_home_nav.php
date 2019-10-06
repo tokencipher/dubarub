02c0c872944af3bbe37747d829ddd0886290b244
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-10-06 17:17:58 -0400

    Add "View All Users" selection to nav

--- php_inc/inc_user_home_nav.php
+++ php_inc/inc_user_home_nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div id="header" class="w3-container w3-white" style="opacity:0.6">
       <img style="position:relative;top:6px" src="img/dubarub.jpg" alt="dubarub" id="dub" height="40" width="47" />
-      <span style="position:relative;top:13px" id="logoTitle"> Closed BETA </span>
+      <span style="position:relative;top:13px" id="logoTitle"> Public BETA </span>
       <div class="dropdown">
         <button class="btn  btn-lg  dropdown-toggle" id="dropdownMenu1" type="button"  aria-label="Dropdown Menu">
         <span class="glyphicon glyphicon-menu-hamburger"></span>
@@ -14,6 +14,7 @@
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="playlist_form.php">Add to Playlist</a></li>
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="playlist_modify.php">Delete from Playlist</a></li>
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="music_form.php">Upload Track(s)</a></li>
+          <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="users.php">View All Users</a></li>
           <li><a target="_self" onmouseover="this.style.color='red'" onmouseout="this.style.color='grey'" href="logout.php?PHPSESSID=<?php echo session_id()?>">Logout</a></li>
           <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./gallery.php">Gallery</a></li>-->
           <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./working.php">Tags</a></li>-->
