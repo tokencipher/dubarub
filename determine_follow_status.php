5c3e6135f0aedb325617547c74f19c38222f479e
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-10-06 17:01:04 -0400

    Determine follow flag based on currently logged in user

--- determine_follow_status.php
+++ determine_follow_status.php
@@ -1,13 +1,22 @@
+<?php session_start(); 
+if (!isset($_SESSION['user_id']) && !isset($_SESSION['logged_in'])) {
+  // User not logged in. Redirect them back to the login.php page.
+  header('Location: index.php');
+  exit;
+}
+?>
 <?php require_once ('php_class/class_Follow.php'); ?>
 <?php
+
   $loggedInUserId = $_POST['logged_in_user_id'];
   $renderedUserId = $_POST['rendered_user_id'];
   $avatar = $_POST['avatar'];
   $userName = $_POST['user_name'];
   $bio = $_POST['bio'];
+  
   // Identify if user profile being viewed is being followed by current logged in user
   $followObj = new Follow();
-  $following = $followObj->getFollowFlag($loggedInUserId, $renderedUserId);
+  $following = $followObj->getFollowFlag($_SESSION['user_id'], $userName);
  
   $followStatus = array();
   $followStatus['follow_status'] = $following;
