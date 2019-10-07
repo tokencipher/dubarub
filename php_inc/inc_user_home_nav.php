b78568063bfe7baf60efeb990685bb09dd34aae3
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-10-07 14:27:11 -0400

    Modify anchor tags' href's

--- php_inc/inc_user_home_nav.php
+++ php_inc/inc_user_home_nav.php
@@ -7,9 +7,9 @@
         <span class="glyphicon glyphicon-menu-hamburger"></span>
         </button>
         <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
-          <li><a onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./home.php">Home</a></li>
+          <li><a onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="home.php">Home</a></li>
           <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./video_preview.php">Video</a></li>-->
-          <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./post_form.php">Add Post</a></li>
+          <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="post_form.php">Add Post</a></li>
           <li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" id="music-link" href="#music-dialog">Playlist</a></li>
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="playlist_form.php">Add to Playlist</a></li>
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="playlist_modify.php">Delete from Playlist</a></li>
