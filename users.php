a3e1f5287fd6bafe75d68a25b86e37d8cef9f0fc
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-10-23 21:47:51 -0400

    Implement functionality for send message and follow/unfollow buttons

--- users.php
+++ users.php
@@ -156,24 +156,24 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['logged_in'])) {
 		      '" width="65" height="65"></img></div><div id="bio_username_container">' + 
 		      '<div id="bio_username">' + data.user_name + '</div></div>' + 
 		      '<div id="bio_action_container">' +
-		      '<i id="direct_message" class="fa fa-paper-plane action_items" ' + 
+		      '<i id="direct_message" onclick="displayMessageModal()" class="fa fa-paper-plane action_items" ' + 
 		      'aria-hidden="true"></i></div><div id="bio_text_container">' + 
 		      '<p id="bio_text">' + data.bio + '</p></div>' + 
-		      '<div id="unfollow_button_container"><a href="#" onclick="unfollow()" ' +
+		      '<div id="unfollow_button_container"><a onclick="unfollow(this)" ' +
 		      'class="btn btn-primary btn-sm btn-block active" role="button" ' + 
-		      'aria-pressed="true">Unfollow</a></div></div>');
+		      'aria-pressed="true" data-rendered-user-id="' + data.rendered_user_id + '">Unfollow</a></div></div>');
             } else {
               var user = $('<div id="user' + data.rendered_user_id + '" ' + 
 		      'class="flex_item"><div id="bio_avi"><img id="avatar" src="' + data.avatar +
 		      '" width="65" height="65"></img></div><div id="bio_username_container">' + 
 		      '<div id="bio_username">' + data.user_name + '</div></div>' + 
 		      '<div id="bio_action_container">' +
-		      '<i id="direct_message" class="fa fa-paper-plane action_items" ' + 
+		      '<i id="direct_message" onclick="displayMessageModal()" class="fa fa-paper-plane action_items" ' + 
 		      'aria-hidden="true"></i></div><div id="bio_text_container">' + 
 		      '<p id="bio_text">' + data.bio + '</p></div>' + 
-		      '<div id="follow_button_container"><a href="#" onclick="follow()" ' + 
+		      '<div id="follow_button_container"><a onclick="follow(this)" ' + 
 		      'class="btn btn-primary btn-sm btn-block active" role="button" ' +
-		      'aria-pressed="true">Follow</a></div></div>');
+		      'aria-pressed="true" data-rendered-user-id="' + data.rendered_user_id + '">Follow</a></div></div>');
             }
 	  
 		    var flexContainer = $('.flex_container');
@@ -190,11 +190,124 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['logged_in'])) {
     usersRequest.send();
 
   }
+  
+  function displayMessageModal() {
+    document.getElementById('message_modal').style.display = 'block';
+  }
+  
+  function follow(elem) {
+    console.log("Follow button clicked");
+    var logged_in = Boolean("<?php echo (isset($_SESSION['user_id']) ? true : false); ?>");
+    	
+    if (logged_in === true) {	
+	  var action = "Follow";
+	  var renderedUserId = $(elem).data("rendered-user-id");
+	  
+	  $(elem).text("Unfollow");
+	  $(elem).parent().attr("id", "unfollow_button_container");
+	  $(elem).attr("onclick", "unfollow(this)");	
+	  
+	
+	  /*
+	  $.ajax({
+        async: true,
+      	cache: false,
+        url: 'user_action.php',  
+        type: 'POST',
+        data: {user_action: action, rendered_user_id: renderedUserId}  
+      }).done(function (msg) {
+        console.log('Follow action taken...');
+        console.log(msg);
+      }).fail(function (xhr, textStatus) {
+        console.log(xhr.statusText);
+      });  
+      */   
+    } else {
+      if (confirm("You must be logged in to follow this user. Sign up/Login?")) {
+  	    window.location.assign("https://dubarub.com");
+	  } else {
+  		return;
+	  }
+    }       
+  }
+  
+  function unfollow(elem) {
+    console.log("Unfollow button clicked");
+    var logged_in = Boolean("<?php echo (isset($_SESSION['user_id']) ? true : false); ?>");
+    
+    if (logged_in === true) {
+      var action = "Unfollow";
+      var renderedUserId = $(elem).data("rendered-user-id");
+      
+	  $(elem).text("Follow");
+	  $(elem).parent().attr("id", "follow_button_container");
+	  $(elem).attr("onclick", "follow(this)");
+      
+      /*
+      $.ajax({
+        async: true,
+        cache: false,
+        url: 'user_action.php',
+        type: 'POST',
+        data: {user_action: action, rendered_user_id: renderedUserId}
+      }).done(function (msg) {
+        console.log("Unfollow action taken");
+        console.log(msg);
+      }).fail(function (xhr, textStatus) {
+        console.log(xhr.statusText);
+	  });
+	  */
+    } else {
+      if (confirm("You must be logged in to unfollow this user. Sign up/Login?")) {
+        window.location.assign("https://dubarub.com");
+      } else {
+        return;
+      }
+    }
+  }
+  
 
   $(document).ready(function() {
     loadUsers();
   });
 </script>
+
+<!-- direct message dialog -->
+<div id="message_modal" class="w3-modal">
+<div class="w3-modal-content w3-card-4 w3-anmiate-zoom">
+  <header class="w3-container w3-blue">
+	<span onclick="document.getElementById('message_modal').style.display = 'none'"
+	class="w3-button w3-red w3-display-topright">&times;</span>
+	<h2>Send message</h2>
+  </header>
+
+  <div class="w3-center"><br>
+	<form class="w3-container" method="post">
+	  <div class="form-group">
+		<div><i class="w3-xxlarge fa fa-paper-plane" style="color:#339966;"></i></div>
+		<!--<label for="post_title">Enter caption of new post</label>-->
+		Subject: <input type="text" maxlength="140" name="message_subject" class="form-control" id="message_subject" aria-describedby="subject_help" placeholder="" />
+		<small id="subject_help" class="form-text text-muted">Subject cannot be any longer than 140 characters.</small>
+	  </div>
+	  <div class="form-group">
+		<!-- <label for="bio_edit">Edit bio</label> -->
+		Body: <textarea id="message_body" name="message_body" class="form-control" placeholder="" rows="3" maxlength="" required></textarea>
+		<br>
+	  </div>
+	</form>
+	
+	<div id="message_submit_container">
+	  <button id="message_submit" class="btn btn-primary w3-margin-bottom">Send</button>
+	</div>
+  </div>
+  
+  <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+	<button onclick="document.getElementById('message_modal').style.display='none'"
+	type="button" class="w3-button w3-left w3-border w3-red">Cancel</button>
+  </div>
+</div>
+</div>
+<!-- end direct message dialog -->
 
 <div class="flex_container"></div>
     
