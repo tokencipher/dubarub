491b193e7d28d83939a249329be5b515aa833142
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-10-29 19:22:07 -0400

    Add feed link to nav menu

--- php_inc/inc_user_nav.php
+++ php_inc/inc_user_nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div id="header" class="w3-container w3-white" style="opacity:0.6">
       <img style="position:relative;top:6px" src="img/dubarub.jpg" alt="dubarub" id="dub" height="40" width="47" />
-      <span style="position:relative;top:13px" id="logoTitle"> Closed BETA </span>
+      <span style="position:relative;top:13px" id="logoTitle"> Public BETA </span>
       <div class="dropdown">
         <button class="btn  btn-lg  dropdown-toggle" id="dropdownMenu1" type="button"  aria-label="Dropdown Menu">
         <span class="glyphicon glyphicon-menu-hamburger"></span>
@@ -9,6 +9,7 @@
         <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
           <li><a onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./home.php">Home</a></li>
           <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./video_preview.php">Video</a></li>-->
+          <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./feed.php">Feed</a></li>
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="./post_form.php">Add Post</a></li>
           <!--<li><a target="_blank" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" id="music-link" href="#music-dialog">Playlist</a></li>-->
           <li><a target="_self" onmouseover="this.style.color='blue'" onmouseout="this.style.color='grey'" href="playlist_form.php">Add to Playlist</a></li>
