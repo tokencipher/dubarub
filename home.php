58b48c2ce4a8064e9d73d59dd806c20af548d184
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2020-01-02 20:52:34 -0500

    Change id's to class attributes for comment section

--- home.php
+++ home.php
@@ -606,23 +606,23 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['logged_in'])) {
               $('#post' + comments[i].p_id).find('.post_comments').append('<div id="comment' + comments[i].c_id +  '" class="media">' +
   			  '<div class="media-left">' + 
   			  '<a href="#"><img style="margin-left:5px" height="64" width="64" class="media-object" src="' + comments[i].avatar + '" alt="user avatar"></a>' +
- 			  '</div><div style="position:relative;top:-5px;text-align:left;" class="media-body"><div id="commenter" style="font-size:14px;" class="media-heading"><b><a id="comment_owner_link" href="user.php?name=' + comments[i].user_name + '">' + comments[i].user_name + '</a></b> says:</div>' + 
- 			  '<div id="comment_body" style="margin-bottom:2px;font-size:12px">' + comments[i].comment + '</div>' + 
+ 			  '</div><div style="position:relative;top:-5px;text-align:left;" class="media-body"><div class="commenter" style="font-size:14px;" class="media-heading"><b><a id="comment_owner_link" href="user.php?name=' + comments[i].user_name + '">' + comments[i].user_name + '</a></b> says:</div>' + 
+ 			  '<div class="comment_body" style="margin-bottom:2px;font-size:12px">' + comments[i].comment + '</div>' + 
  			  '<div onclick="removeComment(this)" data-commid="' + comments[i].c_id + '" id="remove_comment' + comments[i].c_id + '" style="position:relative;bottom:22px;cursor:pointer;margin-right:10px;float:right">' + 
  			  '<i class="fa fa-times" style="color:red" aria-hidden="true"></i></div>' + 
- 			  '<div id="comment_options" style="clear:both;font-size:12px" class="flex-container">' + 
- 			  '<div id="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
- 			  '<div id="upvote">' + comments[i].upvote + " " + trophyAmount + '</div>' +
+ 			  '<div style="clear:both;font-size:12px" class="comment_options flex-container">' + 
+ 			  '<div class="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
+ 			  '<div class="upvote">' + comments[i].upvote + " " + trophyAmount + '</div>' +
  			  '</div></div>');
  			} else {
  			  $('#post' + comments[i].p_id).find('.post_comments').append('<div id="comment' + comments[i].c_id +  '" class="media">' +
   			  '<div class="media-left">' + 
   			  '<a href="#"><img style="margin-left:5px" height="64" width="64" class="media-object" src="' + comments[i].avatar + '" alt="user avatar"></a>' +
- 			  '</div><div style="position:relative;top:-5px;text-align:left;" class="media-body"><div id="commenter" style="font-size:14px;" class="media-heading"><b><a id="comment_owner_link" href="user.php?name=' + comments[i].user_name + '">' + comments[i].user_name + '</a></b> says:</div>' + 
- 			  '<div id="comment_body" style="margin-bottom:2px;font-size:12px">' + comments[i].comment + '</div>' + 
- 			  '<div id="comment_options" style="clear:both;font-size:12px" class="flex-container">' + 
- 			  '<div id="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
- 			  '<div id="upvote">' + comments[i].upvote + " " + trophyAmount + '</div>' +
+ 			  '</div><div style="position:relative;top:-5px;text-align:left;" class="media-body"><div id="commenter' + comments[i].c_id + '" style="font-size:14px;" class="media-heading"><b><a id="comment_owner_link" href="user.php?name=' + comments[i].user_name + '">' + comments[i].user_name + '</a></b> says:</div>' + 
+ 			  '<div class="comment_body" style="margin-bottom:2px;font-size:12px">' + comments[i].comment + '</div>' + 
+ 			  '<div style="clear:both;font-size:12px" class="comment_options flex-container">' + 
+ 			  '<div class="comment_timestamp">' + moment(comments[i].timestamp, "YYYY-MM-DD kk:mm:ss").fromNow() + '</div>' +
+ 			  '<div class="upvote">' + comments[i].upvote + " " + trophyAmount + '</div>' +
  			  '<div onclick="handCommentTrophy(this)" class="comment_trophy" data-commid="' + comments[i].c_id + '"><i class="fa fa-trophy" style="color:#b36b00" aria-hidden="true"></i></a></div>' + 
  			  '<div onclick="flagComment(this)" class="comment_flag" data-commid="' + comments[i].c_id + '" style="position:relative;left:15px;"><i style="color:red" class="fa fa-flag" aria-hidden="true"></i></div>' +
  			  '</div></div>');
