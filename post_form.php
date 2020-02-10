f77cbdbee1f370b431de6cf65a01a8786343001a
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2020-02-10 13:23:46 -0500

    Set maxlength attribute for post text content

--- post_form.php
+++ post_form.php
@@ -93,7 +93,7 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['logged_in'])) {
   </div>
   <div class="form-group">
     <label for="post_text">Now, let your mind flow freely</label>
-    <textarea id="post_text" name="post_text" class="form-control" rows="3" maxlength="" required></textarea>
+    <textarea id="post_text" name="post_text" class="form-control" rows="3" maxlength="5000" required></textarea>
     <small id="post_text_help" class="form-text text-muted">Post text content cannot be any longer than 5000 characters.</small>
   </div>
   <div class="form-group">
