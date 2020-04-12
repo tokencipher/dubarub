9ea6d7f10df6bcc58b3d1ba4baaf82365797e5bc
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2020-04-12 19:40:29 -0400

    Update logic for click of direct_message icon

--- user.php
+++ user.php
@@ -1,5 +1,5 @@
 <!-- Author: Bryan Thomas -->
-<!-- Last modified: 02/08/20 -->
+<!-- Last modified: 04/10/20 -->
 <?php 
   session_start();
   
@@ -913,7 +913,17 @@
     });  
     
     $('#direct_message').click(function() {
-      document.getElementById('message_modal').style.display = 'block';
+      if (logged_in_user) {
+        console.log('User is currently logged in');
+        document.getElementById('message_modal').style.display = 'block';
+      } else {
+        console.log('User is NOT currently logged in');
+        if (confirm("You must be logged in to send this user a message. Sign up/Login?")) {
+          window.location.assign("https://dubarub.com");
+        } else {
+          return false;
+        }
+      }
     }); 
     
     // If current logged in user has the same user id as user id of profile being 
@@ -995,26 +1005,8 @@
 		class="w3-button w3-red w3-display-topright">&times;</span>
 		<h2>Send message</h2>
 	  </header>
-    
-      <div class="w3-center"><br>
-        <form class="w3-container" method="post">
-          <div class="form-group">
-            <div><i class="w3-xxlarge fa fa-paper-plane" style="color:#339966;"></i></div>
-            <!--<label for="post_title">Enter caption of new post</label>-->
-            Subject: <input type="text" maxlength="140" name="message_subject" class="form-control" id="message_subject" aria-describedby="subject_help" placeholder="" />
-            <small id="subject_help" class="form-text text-muted">Subject cannot be any longer than 140 characters.</small>
-          </div>
-          <div class="form-group">
-            <!-- <label for="bio_edit">Edit bio</label> -->
-            Body: <textarea id="message_body" name="message_body" class="form-control" placeholder="" rows="3" maxlength="" required></textarea>
-            <br>
-          </div>
-        </form>
-        
-        <div id="message_submit_container">
-          <button id="message_submit" class="btn btn-primary w3-margin-bottom">Send</button>
-        </div>
-      </div>
+
+      <iframe src="https://dubarub.com:4200/message_form/<?php echo $_SESSION['user_name']; ?>" height="100%" width="100%" style="border:none"></iframe>
       
       <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
         <button onclick="document.getElementById('message_modal').style.display='none'"
