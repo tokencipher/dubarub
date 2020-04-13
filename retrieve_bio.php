298c99f02ecd23906d49d9e7f31f0ce895dfc52d
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2020-04-12 20:19:13 -0400

    Retrieve bio data onclick of bio edit icon

---
+++ retrieve_bio.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/json;charset=utf-8');
+include('php_inc/inc_db_qp4.php');
+
+if (isset($_POST['u_id'])) {
+  $user_id = $_POST['u_id'];
+  
+  /* If you uncomment this segment, escaped bio text will display to user from DB */
+  //$bio = addslashes($bio);
+  
+  if ($conn !== FALSE) {
+	$table = "user";
+	// Retrieve the data
+	$sql = "SELECT u_id, bio FROM $table WHERE u_id=$user_id";
+	$object = array(); 
+	$x = 0;
+	foreach ($conn->query($sql) as $row) {
+	  $object[$x]['u_id'] = "{$row['u_id']}";
+	  $object[$x]['bio'] = "{$row['bio']}";
+	}
+  }
+}
+
+
+// Send the data back to the caller
+$myObj = json_encode($object);
+echo $myObj;
+$conn = null;
+
+?>
